0da325655678b200b79b5a12964f37c4ddfd9bf7
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-05-02 12:26:47 +0100

    Handling form submission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,30 +6,14 @@ import Post from './Post';
 import styles from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={styles.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author='Larry' body='This is from Larry' />
       </ul>
     </>
