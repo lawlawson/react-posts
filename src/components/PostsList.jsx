d558055a3c944effc85864f7c038de670c2a005b
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-05-04 18:22:33 +0100

    Outputted list dynamically

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -19,9 +19,19 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={styles.posts}>
-        <Post author='Larry' body='This is from Larry' />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={styles.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.author} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
